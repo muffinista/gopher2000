4db7a22f1c78cea286ae20b682a36b88796a1df2
Colin Mitchell <muffinista@gmail.com>
2012-01-05 20:04:58 -0500

    routing cleanup

--- lib/gopher/routing.rb
+++ lib/gopher/routing.rb
@@ -1,51 +1,36 @@
 module Gopher
   module Routing
 
-    def route(path, *args, &block)
+    # @todo toss *args or do something with it
+    def route(path, &block)
       selector = self.sanitize_selector(path)
-
-      # @todo toss *args or do something with it
-      sig = compile!(selector, block, {})
-
-      #puts sig.inspect
+      sig = compile!(selector, &block)
 
       @routes ||= []
       @routes << sig
     end
 
-    def compile_route(path)
-      keys = []
-      if path.respond_to? :to_str
-        pattern = path.to_str.gsub(/[^\?\%\\\/\:\*\w]/) { |c| encoded(c) }
-        pattern.gsub!(/((:\w+)|\*)/) do |match|
-          if match == "*"
-            keys << 'splat'
-            "(.*?)"
-          else
-            keys << $2[1..-1]
-            "([^/?#]+)"
-          end
-        end
-        [/^#{pattern}$/, keys]
-      end
+#    def compile!(path, block, options = {})
+#      options.each_pair { |option, args| send(option, *args) }
+    def compile!(path, &block)
+      method_name = path
+      route_method = Dispatching.generate_method(method_name, &block)
+      pattern, keys = compile path
+
+
+      # sinatra does some arity checks when making routes -- not sure we need them
+
+      #      [ pattern, keys, conditions, block.arity != 0 ?
+      #        proc { |a,p| puts "A: #{a}, P: #{p}"; route_method.bind(a).call(*p) } :
+      #        proc { |a,p| route_method.bind(a).call } ]
+
+      [ pattern, keys, route_method ]
     end
 
-    def compile!(path, block, options = {})
-      options.each_pair { |option, args| send(option, *args) }
-      method_name             = path
-      unbound_method          = Dispatching.generate_method(method_name, &block)
-      pattern, keys           = compile path
-
-#      [ pattern, keys, conditions, block.arity != 0 ?
-#        proc { |a,p| puts "A: #{a}, P: #{p}"; unbound_method.bind(a).call(*p) } :
-#        proc { |a,p| unbound_method.bind(a).call } ]
-
-      [ pattern, keys, unbound_method ]
-    end
-
+    protected
     def compile(path)
       keys = []
-      if path.respond_to? :to_str
+#      if path.respond_to? :to_str
         pattern = path.to_str.gsub(/[^\?\%\\\/\:\*\w]/) { |c| encoded(c) }
         pattern.gsub!(/((:\w+)|\*)/) do |match|
           if match == "*"
@@ -57,15 +42,15 @@ module Gopher
           end
         end
         [/^#{pattern}$/, keys]
-      elsif path.respond_to?(:keys) && path.respond_to?(:match)
-        [path, path.keys]
-      elsif path.respond_to?(:names) && path.respond_to?(:match)
-        [path, path.names]
-      elsif path.respond_to? :match
-        [path, keys]
-      else
-        raise TypeError, path
-      end
+      # elsif path.respond_to?(:keys) && path.respond_to?(:match)
+      #   [path, path.keys]
+      # elsif path.respond_to?(:names) && path.respond_to?(:match)
+      #   [path, path.names]
+      # elsif path.respond_to? :match
+      #   [path, keys]
+#      else
+#        raise TypeError, path
+#      end
     end
 
     # Sanitizes a gopher selector
--- spec/routing_spec.rb
+++ spec/routing_spec.rb
@@ -6,24 +6,50 @@ class MockServer
 end
 
 describe Gopher::Routing do
-  before(:all) do
-    @klass = MockServer.new
+  before(:each) do
+    @router = MockServer.new
   end
 
-
-  pending "should work with text output" do
-    @klass.route '/about' do; end
-  end
-
-  pending "should work with input" do
-    @klass.route '/name' do
-      render :hello, input.strip
+  describe "compile" do
+    it "should generate a basic string for routes without keys" do
+      lookup, keys, block = @router.compile! "/foo" do; end
+      lookup.to_s.should == /^\/foo$/.to_s
+      keys.should == []
     end
-  end
 
-  pending "should work with templates" do
-    @klass.route '/' do
-      render :index
+    context "with keys" do
+      it "should generate a lookup and keys for routes with keys" do
+        lookup, keys, block = @router.compile! "/foo/:bar" do; end
+        lookup.to_s.should == "(?-mix:^\\/foo\\/([^\\/?#]+)$)"
+
+        keys.should == ["bar"]
+      end
+
+      it "should match correctly" do
+        lookup, keys, block = @router.compile! "/foo/:bar" do; end
+        lookup.to_s.should == "(?-mix:^\\/foo\\/([^\\/?#]+)$)"
+
+        lookup.match("/foo/baz").should_not be_nil
+        lookup.match("/foo2/baz").should be_nil
+        lookup.match("/baz/foo/baz").should be_nil
+        lookup.match("/foo/baz/bar").should be_nil
+      end
+    end
+
+    context "with splat" do
+      it "should work with splats" do
+        lookup, keys, block = @router.compile! "/foo/*" do; end
+        lookup.to_s.should == "(?-mix:^\\/foo\\/(.*?)$)"
+        keys.should == ["splat"]
+      end
+
+      it "should match correctly" do
+        lookup, keys, block = @router.compile! "/foo/*" do; end
+
+        lookup.match("/foo/baz/bar/bam").should_not be_nil
+        lookup.match("/foo2/baz").should be_nil
+        lookup.match("/baz/foo/baz").should be_nil
+      end
     end
   end
 end
