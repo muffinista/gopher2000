ba1f154b145ed4ddc4217e4fc60d1a1662df5d99
Colin Mitchell <muffinista@gmail.com>
2012-02-12 18:04:40 -0500

    specs

--- lib/gopher/rendering/menu.rb
+++ lib/gopher/rendering/menu.rb
@@ -2,30 +2,36 @@ module Gopher
   module Rendering
     # The MenuContext is for rendering gopher menus
     class Menu < Base
+      NO_HOST = '(FALSE)'
+      NO_PORT = 0
 
       # Sanitizes text for use in gopher menus
       def sanitize_text(raw)
-        text = raw.dup
-        text.rstrip! # Remove excess whitespace
-        text.gsub!(/\t/, ' ' * 8) # Tabs to spaces
-        text.gsub!(/\n/, '') # Get rid of newlines (\r as well?)
-        text
+        # text = raw.dup
+        # text.rstrip! # Remove excess whitespace
+        # text.gsub!(/\t/, ' ' * 8) # Tabs to spaces
+        # text.gsub!(/\n/, '') # Get rid of newlines (\r as well?)
+        # text
+        raw.
+          rstrip. # Remove excess whitespace
+          gsub(/\t/, ' ' * 8). # Tabs to spaces
+          gsub(/\n/, '') # Get rid of newlines (\r as well?)
       end
 
       # Creates a gopher menu line from +type+, +text+, +selector+, +host+ and +port+
       # +host+ and +post+ will default to the current host and port of the running Gopher server
       # (by default 0.0.0.0 and 70)
       # +text+ will be sanitized according to a few simple rules (see Gopher::Utils)
-#      def line(type, text, selector, host=Gopher::Server.host, port=Gopher::Server.port)
+      #      def line(type, text, selector, host=Gopher::Server.host, port=Gopher::Server.port)
       def line(type, text, selector, host=application.host, port=application.port)
         text = sanitize_text(text)
 
+        # no need to add a line-ending here, Base will handle that
         self << ["#{type}#{text}", selector, host, port].join("\t")
-#        self << "\r\n" # End the gopher line
       end
 
       def text(text)
-        line 'i', text, 'null', '(FALSE)', 0
+        line 'i', text, 'null', NO_HOST, NO_PORT
       end
 
       def link(text, selector, *args)
--- spec/rendering/menu_spec.rb
+++ spec/rendering/menu_spec.rb
@@ -3,6 +3,9 @@ require File.join(File.dirname(__FILE__), '/../spec_helper')
 describe Gopher::Rendering::Menu do
   before(:each) do
     @ctx = Gopher::Rendering::Menu.new
+
+    application.stub!(:host).and_return("host")
+    application.stub!(:port).and_return(1234)
   end
 
   it 'should add text as a gopher line' do
@@ -12,11 +15,49 @@ describe Gopher::Rendering::Menu do
     @ctx.result.should == "igopher forever\tnull\t(FALSE)\t0"
   end
 
-  pending "sanitize"
-  
-  pending "line"
-  pending "link"
-  pending "search"
-  pending "menu"
+  describe "sanitize_text" do
+    it "should remove extra whitespace from end of line" do
+      @ctx.sanitize_text("x   ").should == "x"
+    end
+
+    it "should convert tabs to spaces" do
+      @ctx.sanitize_text("x\tx").should == "x        x"
+    end
+
+    it "should remove newlines" do
+      @ctx.sanitize_text("x\nx").should == "xx"
+    end
+  end
+
+  describe "line" do
+    it "should work" do
+      #def line(type, text, selector, host=application.host, port=application.port)
+      @ctx.line("type", "text", "selector", "host", "port").should == "typetext\tselector\thost\tport"
+    end
+
+    it "should use application host/port as defaults" do
+      @ctx.line("type", "text", "selector").should == "typetext\tselector\thost\t1234"
+    end
+  end
+
+  describe "link" do
+    it "should get type with determine_type" do
+      @ctx.should_receive(:determine_type).with("foo.txt").and_return("A")
+      @ctx.link("FILE", "foo.txt").should == "AFILE\tfoo.txt\thost\t1234"
+    end
+  end
+
+  describe "search" do
+    it "should output link type/text" do
+      @ctx.search("FIND", "search").should == "7FIND\tsearch\thost\t1234"
+    end
+  end
+
+  describe "menu" do
+    it "should output link type/text" do
+      @ctx.menu("MENU ITEM", "item").should == "1MENU ITEM\titem\thost\t1234"
+    end
+  end
+
   pending "determine_type"
 end
